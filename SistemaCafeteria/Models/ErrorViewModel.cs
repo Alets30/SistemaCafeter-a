c9a001bee2eaf2d5138bc6d9e6b6369d01830b8a
Alets30 <ettosn30@gmail.com>
2024-02-09 07:54:37 -0600

    Mi primer commit

---
+++ SistemaCafeteria/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaCafeteria.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
